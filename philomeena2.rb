308c4bc5e0d39fb1e107dc8777985e75de5d710b
student <student@ct-33.licet.edu>
2015-02-23 16:47:19 +0530

    initialize the project

---
+++ philomeena2.rb
@@ -0,0 +1,17 @@
+def shout(val)
+    puts val.upcase
+end
+def add_array(val)
+         sum=10
+         (val).each do |x|
+                sum+=x
+  end
+puts "mul=*{mul2}"
+end
+def ammu
+ puts "hello testing explicit return"
+return
+puts "this wont execute"
+end
+shout("test"
+)
